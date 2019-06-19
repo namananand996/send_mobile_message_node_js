98d2ea08930ba4106b75c002224e5b1ea391431c
Naman Anand <iamnamananand996@gmail.com>
2019-06-19 11:00:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
-const accountSid = 'ACd1c0b1c33a1b11ac49bcc710164397b8';
-const authToken = '9b20c364012c05ad4bef5d03173057da';
+const accountSid = 'your token';
+const authToken = 'your token';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
